6898294376475511595fa3c7000f5f1503252e8e
Jonatan Bengtsson <145494345+JonatanBengtsson94@users.noreply.github.com>
2023-11-01 12:34:02 +0100

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -1,13 +1,52 @@
 #! /bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=salon -t -c"
+PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -A -c"
+SERVICE_ID_SELECTED=""
+SERVICE_NAME=""
 
-MAIN_MENU(){
-    echo -e "\nMAIN MENU\n"
-    $PSQL "SELECT * FROM services" | while read SERVICE_ID NAME
+MAIN_MENU() {
+    while true
     do
-        echo $SERVICE_ID") " $NAME
+        # Show the main menu
+        # echo -e "\nMAIN MENU\n"
+        $PSQL "SELECT * FROM services" | while IFS="|" read -r SERVICE_ID SERVICE_NAME
+        do
+            echo -e "$SERVICE_ID) $SERVICE_NAME"
+        done
+
+        # Chose a service
+        read SERVICE_ID_SELECTED
+        case $SERVICE_ID_SELECTED in
+            1|2|3) SERVICE_NAME=$($PSQL "SELECT name FROM services where service_id = $SERVICE_ID_SELECTED")
+            echo "Selected service: $SERVICE_NAME"
+            break
+            ;;
+            *) MAIN_MENU  ;;
+        esac
     done
 }
 
 MAIN_MENU
+
+
+# Enter phone number
+echo "Enter phone number"
+read CUSTOMER_PHONE
+
+# Check if the customer exists
+CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers where phone = '$CUSTOMER_PHONE'")
+if [[ -z $CUSTOMER_ID ]]
+then
+    echo "Enter your name"
+    read CUSTOMER_NAME
+    $PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')"
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers where phone = '$CUSTOMER_PHONE'")
+else
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers where phone = '$CUSTOMER_PHONE'")
+fi
+echo "Enter time"
+read SERVICE_TIME
+$PSQL "INSERT INTO appointments(customer_id, service_id) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED)"
+
+echo -e "I have put you down for a $NAME at $SERVICE_TIME, $CUSTOMER_NAME."
+
